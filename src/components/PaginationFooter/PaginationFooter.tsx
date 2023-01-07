beb7f2e63587d9c1b586eeca30fb143c99caf5c5
julio.guella <julio.guella@artekium.com>
2023-01-07 00:18:29 -0300

    style arrows

--- src/components/PaginationFooter/PaginationFooter.tsx
+++ src/components/PaginationFooter/PaginationFooter.tsx
@@ -28,40 +28,51 @@ export const PaginationFooter: React.FC<PaginationFooterProps> = ({
 
   return (
     <div className="pagination-container">
-      {showUI &&
-      <ul className="pagination">
-        <li
-          onClick={() => paginate(currentPage - 1)}
-          className="page-number-arrow"
-          tabIndex={0}
-          onKeyUp={(e) => e.key === "Enter" && (() => paginate(currentPage - 1))}
-        >
-          {t("footer.previous")}
-        </li>
-        {(maxShowablePages === undefined ||
-          pageNumbers.length <= maxShowablePages) &&
-          pageNumbers.map((number) => (
-            <li
-              key={number}
-              tabIndex={0}
-              className={
-                currentPage === number ? "page-current-number" : "page-number"
-              }
-              onClick={() => paginate(number)}
-              onKeyUp={(e) => e.key === "Enter" && paginate(number)}
-            >
-              {number}
-            </li>
-          ))}
-        <li
-          onClick={(() => paginate(currentPage + 1))}
-          className="page-number-arrow"
-          tabIndex={0}
-          onKeyUp={(e) => e.key === "Enter" && (() => paginate(currentPage + 1))}
-        >
-          {t("footer.next")}
-        </li>
-      </ul>}
+      {showUI && (
+        <ul className="pagination">
+          <li
+            onClick={() => paginate(currentPage - 1)}
+            className={
+              currentPage === 1 ? "disabled-arrow" : "page-number-arrow"
+            }
+            tabIndex={0}
+            onKeyUp={(e) =>
+              e.key === "Enter" && (() => paginate(currentPage - 1))
+            }
+          >
+            {t("footer.previous")}
+          </li>
+          {(maxShowablePages === undefined ||
+            pageNumbers.length <= maxShowablePages) &&
+            pageNumbers.map((number) => (
+              <li
+                key={number}
+                tabIndex={0}
+                className={
+                  currentPage === number ? "page-current-number" : "page-number"
+                }
+                onClick={() => paginate(number)}
+                onKeyUp={(e) => e.key === "Enter" && paginate(number)}
+              >
+                {number}
+              </li>
+            ))}
+          <li
+            onClick={() => paginate(currentPage + 1)}
+            className={
+              currentPage === Math.ceil(totalDrinks / drinksPerPage)
+                ? " disabled-arrow"
+                : "page-number-arrow"
+            }
+            tabIndex={0}
+            onKeyUp={(e) =>
+              e.key === "Enter" && (() => paginate(currentPage + 1))
+            }
+          >
+            {t("footer.next")}
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
